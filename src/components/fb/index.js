f0681c4e9b7733eaa0afe60be0fdda21e06f38dd
carloscba <carloscba1@gmail.com>
2017-06-07 18:25:24 -0300

    To prevent duplicate firebase.initializeApp()

--- src/components/fb/index.js
+++ src/components/fb/index.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import * as firebase from 'firebase'
 import firebaseConfig from './firebaseConfig'
 
+firebase.initializeApp(firebaseConfig)
+
 class Fb extends Component{
-
+    
     login = () => {
-
-        firebase.initializeApp(firebaseConfig);        
+        
         const provider = new firebase.auth.FacebookAuthProvider();
-
+        
         firebase.auth().signInWithPopup(provider).then(function(result) {
             var token = result.credential.accessToken;
             var user = result.user;
