e3f9d5e0815b46712204862368bc356d48009b60
Carlos Cordoba <carloscba1@gmail.com>
2017-06-12 19:41:43 -0300

    Video component is recording and uploading video to firebase

--- src/components/fb/index.js
+++ src/components/fb/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import * as firebase from 'firebase'
-import firebaseConfig from './firebaseConfig'
+import firebaseConfig from '../../config/firebaseConfig'
 
 firebase.initializeApp(firebaseConfig)
 
--- src/components/usermedia/index.js
+++ src/components/usermedia/index.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import * as firebase from 'firebase'
+import firebaseConfig from '../../config/firebaseConfig'
 
 class UserMedia extends Component{
-    
+       
     constructor(){
         super();
+        this.mediaRecorder;
 
         if (navigator.mediaDevices !== undefined) {
             this.setUserMedia()
@@ -31,6 +34,27 @@ class UserMedia extends Component{
         }
 
         navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+            this.mediaRecorder = new MediaRecorder(stream);
+            this.mediaRecorder.onstop = function(e) {
+                console.log('onstop')
+            }
+            this.mediaRecorder.ondataavailable = function(e) {
+                var auth =  firebase.auth();
+                var storageRef = firebase.storage().ref();
+                var uploadTask = storageRef.child('video/video.webm').put(e.data);
+                uploadTask.on('state_changed', function (snapshot) {
+                    console.info(snapshot);
+                }, function (error) {
+                    console.error(error);
+                }, function () {
+                    var downloadURL = uploadTask.snapshot.downloadURL;
+                    console.log(downloadURL);
+                });                
+
+            }
+
+            console.log('this.mediaRecorder', typeof(this.mediaRecorder));
+
             const videoPlayer = document.getElementById('videoPlayer');
             
             if ("srcObject" in videoPlayer) {
@@ -46,18 +70,35 @@ class UserMedia extends Component{
                 })
                 videoPlayer.play();
             }.bind(this);  
-                      
+
         }.bind(this)).catch(function(err) {
             /* handle the error */
         });
     }
 
+    startRecord(){
+        console.log('this.mediaRecorder', this.mediaRecorder);
+        if(typeof(this.mediaRecorder) === 'object'){
+            this.mediaRecorder.start();
+        }
+        
+        console.log('startRecord');
+    }
+    stopRecord(){
+        if(typeof(this.mediaRecorder) === 'object'){
+            this.mediaRecorder.stop();
+        }
+        
+        console.log('stopRecord');
+    }    
     render(){
+
         return(
             <div>
                 <h1>Media</h1>
                 <video id="videoPlayer"></video>
-                <button disabled = { this.state.recDisabled }>Rec</button>
+                <button onClick={ this.startRecord.bind(this) } disabled = { this.state.recDisabled }>Rec</button>
+                <button onClick={ this.stopRecord.bind(this) } disabled = { this.state.recDisabled }>Stop</button>
             </div>
         )
     }
