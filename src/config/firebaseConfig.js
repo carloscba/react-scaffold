1de3a23b543892df28b651f629c1864da694d287
Carlos Cordoba <carloscba1@gmail.com>
2017-06-14 18:32:59 -0300

    Delete firebaseConfig.js

--- src/config/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyAyrAm4BA3BdROJILFf11QxK9DcZ_lPrRw",
-    authDomain: "react-scaffold.firebaseapp.com",
-    databaseURL: "https://react-scaffold.firebaseio.com",
-    projectId: "react-scaffold",
-    storageBucket: "react-scaffold.appspot.com",
-    messagingSenderId: "486676752234"
-}
-export default firebaseConfig;
